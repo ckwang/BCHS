164862ee50db43b728db570fcb1cd9f541ff6f39
Boon Teik Ooi <ooiboonteik@gmail.com>
2012-01-25 15:17:55 -0500

    analyze possible trip and 2pairs

--- src/util/Hand.java
+++ src/util/Hand.java
@@ -19,34 +19,28 @@ public class Hand {
 	public Card[] hole = new Card[2];
 	public List<Card> community;
 	public List<Card> bestHand;
-	public int[] suits = {0, 0, 0, 0};
-	public int[] ranks = new int[13];
+	public int[] suitCount = {0, 0, 0, 0};
+	public int[] rankCount = new int[13];
 	private Set<Integer> usedCards = new HashSet<Integer>();
 
 	public Hand(Card c1, Card c2) {
 		hole[0] = c1;
 		hole[1] = c2;
-		Arrays.fill(ranks, 0);
+		Arrays.fill(rankCount, 0);
 		if (c1.r == c2.r) {
 			bestCategory = Category.Pair;
 			pocket = true;
 			pairRank1 = c1.r;
 		}
 		community = new ArrayList<Card>();
-		ranks[c1.r] += 1;
-		ranks[c2.r] += 1;
-		suits[c1.s] += 1;
-		suits[c2.s] += 1;
-		
 		usedCards.add(c1.toValue());
 		usedCards.add(c2.toValue());
 	}
 
 	public void addCards(Card c) {
 		community.add(c);
-		assert c.r >= 0 && c.r <= 12;
-		ranks[c.r] += 1;
-		suits[c.s] += 1;
+		rankCount[c.r] += 1;
+		suitCount[c.s] += 1;
 		usedCards.add(c.toValue());
 	}
 
@@ -134,7 +128,7 @@ public class Hand {
 	public void analyzeHand() {
 		if (flushSuit != -1) {
 			for (int i = 0; i < 4; ++i) {
-				if (suits[i] >= 5) {
+				if (suitCount[i] >= 5) {
 					flushSuit = i;
 					break;
 				}
@@ -142,11 +136,11 @@ public class Hand {
 		}
 		int c = 0;
 		for (int i = 0; i < 13; ++i) {
-			if (ranks[i] == 0)
+			if (rankCount[i] == 0)
 				c = 0;
 			else {
 				++c;
-				switch (ranks[i]) {
+				switch (rankCount[i]) {
 					case 4:
 						quadRank = i;
 						bestCategory = Category.Quads;
@@ -170,15 +164,8 @@ public class Hand {
 		}
 	}
 	
-	// TODO: not done yet
 	public List<Card> analyzePossibleStraightFlush() {
 		List<Card> result = new ArrayList<Card>();
-		int[] suitCount = new int[4];
-		
-		for (Card c : community) {
-			suitCount[c.s]++;
-		}
-		
 		for (int i = 0; i < 4; i++) {
 			switch (suitCount[i]) {
 			case 5:
@@ -200,12 +187,6 @@ public class Hand {
 	
 	public List<Card> analyzePossibleQuads() {
 		List<Card> result = new ArrayList<Card>();
-		int[] rankCount = new int[13];
-		
-		for (Card c : community) {
-			rankCount[c.r]++;
-		}
-		
 		for (int i = 0; i < 13; i++) {
 			boolean[] suit = new boolean[4];
 			for (Card c : community) {
@@ -217,32 +198,25 @@ public class Hand {
 			switch (rankCount[i]) {
 			case 4:
 				// TODO: add the case for 4 cards with same rank
-				
 				break;
 			case 3:
 				for (int j = 0; j < 4; j++) {
 					if (suit[j])	continue;
 					int first = Card.toValue(j, i);
-					
 					for (int k = 0; k < 52; k++) {
 						if (usedCards.contains(k) || k == first)	continue;
-						
 						result.add(new Card(j, i));
 						result.add(new Card(k));
 					}
 				}
-				
 				break;
 			case 2:
 				if (hole[0].r == i || hole[1].r ==i)	break;
 				
 				for (int j = 0; j < 4; j++) {
 					if (suit[j])	continue;
-					
 					result.add(new Card(j, i));
-				}
-				
-				break;
+				} break;
 			default:
 				continue;
 			}			
@@ -253,18 +227,11 @@ public class Hand {
 
 	public List<Card> analyzePossibleFullHouse() {
 		List<Card> result = new ArrayList<Card>();
-		int[] rankCount = new int[13];
-		
-		for (Card c : community) {
-			rankCount[c.r]++;
-		}
-		
 		for (int i = 0; i < 13; i++) {	// long
 			for (int j = 0; j < 13; j++) {	// short
 				if (i == j)	continue;
 				int necessaryLong = Math.max(3 - rankCount[i], 0);
 				int necessaryShort = Math.max(2 - rankCount[i], 0);
-				
 				switch (necessaryLong + necessaryShort) {
 				case 0:
 					// TODO: what to do if already in this category
@@ -290,18 +257,11 @@ public class Hand {
 				}
 			}
 		}
-		
 		return result;
 	}
 
 	public List<Card> analyzePossibleFlush() {
 		List<Card> result = new ArrayList<Card>();
-		int[] suitCount = new int[4];
-		
-		for (Card c : community) {
-			suitCount[c.s]++;
-		}
-		
 		for (int i = 0; i < 4; i++) {	
 			switch (suitCount[i]) {
 			case 5:
@@ -322,7 +282,6 @@ public class Hand {
 					if (usedCards.contains(Card.toValue(i, j)))	continue;
 					for (int k = j + 1; k < 13; k++) {
 						if (usedCards.contains(Card.toValue(i, k)))	continue;
-						
 						result.add(new Card(i, j));
 						result.add(new Card(i, k));
 					}
@@ -338,7 +297,6 @@ public class Hand {
 
 	public List<Card> analyzePossibleStraight() {
 		List<Card> result = new ArrayList<Card>();
-		
 		// from 2 
 		for (int i = 0; i <= 8; i++) {
 			boolean[] occupied = new boolean[5];
@@ -401,15 +359,55 @@ public class Hand {
 				continue;
 			}
 		}
-		
 		return result;
 	}
 
-//	public List<Card> analyzePossibleTriplet() {
-//		
-//	}
-//	
-//	public List<Card> analyzePossibleTwoPairs() {
-//		
-//	}
+	public List<Card> analyzePossibleTriplet() {
+		List<Card> result = new ArrayList<Card>();
+		for (int i = 0; i < 13; ++i ) {
+			if (rankCount[i] == 1) {
+				result.add(new Card(-1, i);
+				result.add(new Card(-1, i);
+			}
+			else if (rankCount[i] == 2) {
+				result.add(new Card(-1, i);
+				result.add(new Card(-1, -1);
+			}
+			else if (rankCount[i] >= 3) {
+				return null;
+			}
+		}
+		return result;
+	}
+	
+	// have not eliminate possibilities of getting full hourse or triplets
+	public List<Card> analyzePossibleTwoPairs() {
+		List<Card> result = new ArrayList<Card>();
+		List<int> pairs = new ArrayList<Card>();
+		List<int> singles = new ArrayList<Card>();
+		for (int i = 0; i < 13; ++i ) {
+			if (rankCount[i] == 1) {
+				singles.add(i);
+			}
+			else if (rankCount[i] >= 2) {
+				pairs.add(i);
+			}
+		}
+		if (singles.size() > 1) {
+			for (int i = 0; i < singles.size(); ++i) {
+				for (int j = i; j < singles.size(); ++j) {
+					result.add(new Card(-1, i));
+					result.add(new Card(-1, j);
+				}
+			}
+		}
+		if (pairs.sie() == 1) {
+			for (int i: singles) {
+				result.add(new Card(-1, i);
+				result.add(new Card(-1, -1);
+			}
+		}
+		else
+			return null
+	}
 }
